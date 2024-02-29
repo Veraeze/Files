7610c15faf365759b2faddaf95a12a3bf89d57cc
Vera Ezeagu <Veraeze18@gmail.com>
2024-02-29 11:27:09 +0100

    json and text file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Product> products = new ArrayList<>();
+        createTextFile("C:\\Users\\USER\\IdeaProjects\\Files\\src\\productList", "product.txt");
+        createJsonFile("C:\\Users\\USER\\IdeaProjects\\Files\\src\\productList", "product.json");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Product product1 = new Product("1", "coke", BigDecimal.valueOf(1000), 2);
+        Product product2 = new Product("2", "chocolate", BigDecimal.valueOf(1500), 5);
+        Product product3 = new Product("3", "pepsi", BigDecimal.valueOf(200), 1);
+        Product product4 = new Product("4", "honey", BigDecimal.valueOf(500), 3);
+
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
+        writeToTextFile(products);
+        writeToJsonFile(products);
+
+
+    }
+
+    public static void writeToTextFile(List<Product> products) {
+        StringBuilder list = new StringBuilder("");
+        String location = "C:\\Users\\USER\\IdeaProjects\\Files\\src\\productList\\product.txt";
+        for (Product product : products) {
+            list.append(product);
+        }
+        try (FileOutputStream fileOutputStream = new FileOutputStream(location)) {
+            String data = "id      name        price       quantity" + list;
+            byte[] bytesFormOFTheData = data.getBytes();
+            fileOutputStream.write(bytesFormOFTheData);
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void createTextFile(String location, String filename) {
+        Path path = Path.of(location, filename);
+        try {
+            Files.createFile(path);
+        } catch (IOException exception) {exception.printStackTrace();        }
+    }
+
+    public static void writeToJsonFile(List<Product> products) {
+        ObjectMapper mapper = new ObjectMapper();
+        try (FileOutputStream writer = new FileOutputStream("C:\\Users\\USER\\IdeaProjects\\Files\\src\\productList\\product.json")) {
+            mapper.writeValue(writer, products);
+        } catch (IOException exception) {exception.printStackTrace();}
+    }
+
+    public static void createJsonFile(String location, String filename) {
+        Path path = Path.of(location, filename);
+        try {
+            Files.createFile(path);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
+
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Product {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Product implements Serializable {
+    private String id;
+    private String name;
+    private BigDecimal price = BigDecimal.ZERO;
+    private int quantity;
+
+
+    public Product() {
+    }
+
+    public Product(String id, String name, BigDecimal price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+               \n %s      %s          %f          %d\n""", id, name, price, quantity);
+    }
 }
